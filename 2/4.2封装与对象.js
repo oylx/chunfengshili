d54e1869a19d1c14ba083da9078ec343dcbf5df6
oylx <oylx2008@163.com>
2021-04-12 17:13:45 +0800

    update:封装与对象

--- 2/4.2封装与对象.js
+++ 2/4.2封装与对象.js
@@ -11,4 +11,54 @@
   }
   window.pop = pop;
 })()
-var pop1 = new pop('infoPop');
+// var pop1 = new pop('infoPop');
+
+// Vue的初始化,建造者模式
+// function Vue(options) {
+//   if(!(this instanceof Vue)) {
+//     console.warn('Vue instance')
+//   }
+//   this._init(options)
+// }
+// initMixin(Vue);
+// stateMixin(Vue);
+// eventsMixin(Vue);
+// lifeCircleMixin(Vue);
+// renderMixin(Vue);
+
+// 单例模式示例：写一个全局数据存储对象
+function store() {
+  this.store = {}
+  if(store.install) {
+    return store.install
+  }
+  store.install = this
+}
+store.install = null
+let s1 = new store()
+let s2 = new store()
+s1.store.a = 1
+console.log(s2);
+
+// vue-router如何全局唯一
+let _Vue
+function install(Vue){
+  if(install.installed && _Vue === Vue) return
+  // 注册代码
+  install.installed = true
+  _Vue = Vue
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
