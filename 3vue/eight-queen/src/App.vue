1a029f58061d9a8e7ccf5ffdefa921704380388f
oylx <oylx2008@163.com>
2021-05-13 11:35:44 +0800

    update:ref/render

--- 3vue/eight-queen/src/App.vue
+++ 3vue/eight-queen/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <eight-queen></eight-queen>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EightQueen from '@/components/EightQueen';
 
 export default {
   name: 'App',
   components: {
+    EightQueen,
     HelloWorld
   }
 }
---
+++ 3vue/eight-queen/src/components/EightQueen.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="title">八皇后问题</div>
+    <div class="grid">
+      <div class="row" v-for="(row,r_index) in grids" :key="r_index">
+        <div class="cell" v-for="cell in row" :key="cell.key">
+          Q
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const grids = new Array(8).fill(1).map((_, r) => {
+  return new Array(8).fill(1).map((_, c) => {
+    return {
+      key: `key-${r * 8 + c}`,
+      ok: false,
+    };
+  });
+});
+export default {
+  name: 'EightQueen',
+  data() {
+    return {
+      grids,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
