87e6fdb5e0ef975ab9f165a57b88db1c7bb9ea50
oylx <oylx2008@163.com>
2021-05-17 16:39:24 +0800

    update:select单选&&多选

--- 3vue/eight-queen/src/components/SSelect.vue
+++ 3vue/eight-queen/src/components/SSelect.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <!--    单选-->
+    <select v-model="selected1">
+      <option :value="value" v-for="{value,name} in list" :key="value">{{ name }}</option>
+    </select>
+    <div>
+      当前选择：{{ selected1 }}
+    </div>
     <!--    shift按住多选-->
     <select v-model="selected" multiple>
       <option :value="value" v-for="{value,name} in list" :key="value">{{ name }}</option>
@@ -26,6 +33,7 @@ export default {
         name: '梨子',
       }],
       selected: [],
+      selected1: 'A',
     };
   },
 };
