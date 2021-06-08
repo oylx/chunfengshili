7f3140cf697f588369419f4013ce17c179494a97
oylx <oylx2008@163.com>
2021-06-08 14:30:22 +0800

    mixin

---
+++ 3vue/eight-queen/src/components/SMixin.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <input type="text" @blur="blur">
+  {{errMsg}}
+</div>
+</template>
+
+<script>
+import validateMixin from '@/mixin/validateMixin';
+export default {
+  name: 'SMixin',
+  data: () => ({
+    // errMsg: ''
+  }),
+  mixins: [validateMixin],
+  methods: {
+    validate() {
+      console.log(1);
+    },
+    blur() {
+      this.validate();
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
