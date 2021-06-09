8cc2bf1b50a9d549f35c6faaeeeadf4af7768bc2
oylx <oylx2008@163.com>
2021-06-09 14:53:42 +0800

    模拟响应式源码-优化dep

--- 3vue/4.1mockVuePrinciple.js
+++ 3vue/4.1mockVuePrinciple.js
@@ -6,25 +6,48 @@ let active;
 let onXChanged = cb => {
   active = cb;
   active();
+  active = null;
 };
 
+class Dep {
+  deps = new Set();
+
+  depend() {
+    if (active) {
+      this.deps.add(active);
+    }
+  }
+
+  notify() {
+    this.deps.forEach(dep => dep());
+  }
+}
+
 let ref = initValue => {
+  // 2个闭包
   let value = initValue;
+  let dep = new Dep();
+
   return Object.defineProperty({}, 'value', {
     get() {
+      dep.depend();
       return value;
     },
     set(newValue) {
       value = newValue;
-      active();
+      dep.notify();
     },
   });
 };
 
-x = ref(1);
-onXChanged(() => {
-  y = f(x.value);
-  console.log(y);
-});
+init = () => {
+  x = ref(1);
+  let activeCallback = () => {
+    y = f(x.value);
+    console.log(y);
+  }
+  onXChanged(activeCallback);
+};
+init();
 x.value = 2;
 x.value = 3;
