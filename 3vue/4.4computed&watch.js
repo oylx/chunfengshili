e8ade3ab07bf25edb96892d334f94ac55cf77e8b
oylx <oylx2008@163.com>
2021-06-09 17:50:56 +0800

    computed watch 源码

---
+++ 3vue/4.4computed&watch.js
@@ -0,0 +1,154 @@
+// 1. let x = computed(() => {count.value + 3;});
+// 2.watch(() => count.value, (newValue, oldValue) => {}, { immediate: true });
+// 3.let stop = watchEffect(() => count.value+3)
+
+let active;
+let queue = [];
+
+let effect = (fn, options = {}) => {
+  let effect = (...args) => {
+    try {
+      active = effect;
+      return fn(...args);
+    } finally {
+      active = null;
+    }
+  };
+  effect.options = options;
+  effect.deps = [];
+  return effect;
+};
+
+let cleanUpEffect = effect => {
+  const { deps } = effect;
+  if (deps.length) {
+    for (let i = 0; i < deps.length; i++) {
+      deps[i].delete(effect);
+    }
+  }
+};
+
+let watchEffect = cb => {
+  let runner = effect(cb);
+  runner();
+  return () => {
+    cleanUpEffect(runner);
+  };
+};
+
+let nextTick = cb => Promise.resolve().then(cb);
+
+let queueJob = job => {
+  if (!queue.includes(job)) {
+    queue.push(job);
+    nextTick(flushJobs);
+  }
+};
+
+let flushJobs = () => {
+  let job;
+  while ((job = queue.shift()) !== undefined) {
+    job();
+  }
+};
+
+class Dep {
+  deps = new Set();
+
+  depend() {
+    if (active) {
+      this.deps.add(active);
+      active.deps.push(this.deps);
+    }
+  }
+
+  notify() {
+    this.deps.forEach(dep => queueJob(dep));
+    this.deps.forEach(dep => {
+      dep.options && dep.options.schedular && dep.options.schedular();
+    });
+  }
+}
+
+let ref = initValue => {
+  let value = initValue;
+  let dep = new Dep();
+  return Object.defineProperty({}, 'value', {
+    get() {
+      dep.depend();
+      return value;
+    },
+    set(newValue) {
+      value = newValue;
+      dep.notify();
+    },
+  });
+};
+
+let computed = fn => {
+  let value;
+  let dirty = true;
+  let runner = effect(fn, {
+    schedular: () => {
+      if (!dirty) {
+        dirty = true;
+      }
+    },
+  });
+  return {
+    get value() {
+      if (dirty) {
+        value = runner();
+        dirty = false;
+      }
+      return value;
+    },
+  };
+};
+
+let watch = (source, cb, options = {}) => {
+  let { immediate } = options;
+  const getter = () => {
+    return source();
+  };
+  let oldValue;
+  const runner = effect(getter, {
+    schedular: () => applyCb(),
+  });
+
+  const applyCb = () => {
+    let newValue = runner();
+    if (newValue !== oldValue) {
+      cb(newValue, oldValue);
+      oldValue = newValue;
+    }
+  };
+  if (immediate) {
+    applyCb();
+  } else {
+    oldValue = runner();
+  }
+};
+
+let count = ref(0);
+let computedValue = computed(() => count.value + 3);
+
+document.getElementById('add').addEventListener('click', () => {
+  count.value++;
+});
+let str = '';
+let stop = watchEffect(() => {
+  str = `hello ${count.value} ${computedValue.value}`;
+  document.getElementById('app').innerText = str;
+});
+setTimeout(() => {
+  stop();
+}, 3000);
+watch(() => count.value, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+}, {
+  immediate: true,
+});
+
+
+
