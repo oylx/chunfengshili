919657a2046026b906123acf4f58a7732b66a01b
oylx <oylx2008@163.com>
2021-06-15 16:42:57 +0800

    vue-router demo

--- 3vue/vue-router/src/App.vue
+++ 3vue/vue-router/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/bar">Bar</router-link>
+    <router-link to="/foo">Foo</router-link>
+    <router-view>
+    </router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
