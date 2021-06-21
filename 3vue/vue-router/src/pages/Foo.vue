c4afb63c4d175b96a41bfe1cf0e22d57f6da9dc2
oylx <oylx2008@163.com>
2021-06-21 17:52:42 +0800

    test 路由守卫

--- 3vue/vue-router/src/pages/Foo.vue
+++ 3vue/vue-router/src/pages/Foo.vue
@@ -5,6 +5,18 @@
 <script>
 export default {
   name: 'Foo',
+  beforeRouteEnter(to, from, next) {
+    console.log('foo::beforeRouteEnter:5', to, from);
+    next()
+  },
+  beforeRouteUpdate(to, from, next) {
+    console.log('foo::beforeRouteUpdate:6', to, from);
+    next()
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('foo::beforeRouteLeave:7', to, from);
+    next()
+  }
 };
 </script>
 
--- 3vue/vue-router/src/router.js
+++ 3vue/vue-router/src/router.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+// import Router from './router/router';
 import Router from 'vue-router';
 import Foo from '@/pages/Foo';
 import Bar from '@/pages/Bar';
@@ -8,17 +9,27 @@ const router = new Router({
   routes: [{
     path: '/foo',
     component: Foo,
+    beforeEnter(to, from, next) {
+      console.log('/foo:beforeEnter:4', to, from);
+      next();
+    }
   }, {
     path: '/bar',
     component: Bar,
   }],
 });
 
-router.beforeEach((to, from) => {
-  console.log('beforeEach:', to, from);
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach:1', to, from);
+  next();
+});
+
+router.beforeResolve((to, from, next) => {
+  console.log('beforeResolve:2', to, from);
+  next();
 })
 
 router.afterEach((to, from) => {
-  console.log('afterEach:', to, from);
-})
+  console.log('afterEach:3', to, from);
+});
 export default router;
