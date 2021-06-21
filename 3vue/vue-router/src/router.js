d536810cd713509afdcb4cb885e058325d6cee49
oylx <oylx2008@163.com>
2021-06-21 17:41:53 +0800

    test 路由守卫

--- 3vue/vue-router/src/router.js
+++ 3vue/vue-router/src/router.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
-import Router from './router/router';
+import Router from 'vue-router';
 import Foo from '@/pages/Foo';
 import Bar from '@/pages/Bar';
 
 Vue.use(Router);
-export default new Router({
+const router = new Router({
   routes: [{
     path: '/foo',
     component: Foo,
-  },{
+  }, {
     path: '/bar',
-    component: Bar
+    component: Bar,
   }],
 });
+
+router.beforeEach((to, from) => {
+  console.log('beforeEach:', to, from);
+})
+
+router.afterEach((to, from) => {
+  console.log('afterEach:', to, from);
+})
+export default router;
