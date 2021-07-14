fdd70c76a89ebe10d23172d6ebf6644e2967c7c0
oylx <oylx2008@163.com>
2021-07-14 17:42:31 +0800

    更新$toast说明

--- 3vue/eight-queen/src/common/toast/index.js
+++ 3vue/eight-queen/src/common/toast/index.js
@@ -1,10 +1,17 @@
 import Toast from './MbsToast';
-import { titleMap, iconsMap } from './config'
+import { titleMap, iconsMap } from './config';
 
 export const ToastPlugin = {
   install(Vue) {
     const ToastConstructor = Vue.extend(Toast);
-    Vue.prototype.$toast = toast;
+    Vue.prototype.$toast = function () {
+      if (!arguments[0]) return;
+      const propsData = buildProps(arguments);
+      const instance = new ToastConstructor({ propsData });
+      // 注意{ propsData, methods }等价于{ propsData: propsData, methods: methods }
+      // 打印出{ propsData }为: {propsData: { autoClose: true, message: "操作成功"} }
+      document.body.appendChild(instance.$mount().$el);
+    };
 
     function buildProps(args) {
       let props = {};
@@ -16,14 +23,7 @@ export const ToastPlugin = {
       }
       props.title = titleMap[props.type];
       props.icon = iconsMap[props.type];
-      return props
-    }
-
-    function toast() {
-      if (!arguments[0]) return;
-      const propsData = buildProps(arguments);
-      const instance = new ToastConstructor({ propsData });
-      document.body.appendChild(instance.$mount().$el);
+      return props;
     }
 
   },
